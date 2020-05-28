e1284742c9a4cb907127604b83287ea2344382e6
Luuc van der Zee <luuc_v_d_zee@hotmail.com>
2020-05-28 21:13:49 +0200

    feat: song sections now handled properly

--- cleanRawSongs.js
+++ cleanRawSongs.js
@@ -1,7 +1,7 @@
 import { readFileSync } from 'fs'
 import { COLUMN_JOIN_STRING, ROW_JOIN_STRING } from './joinStrings.js'
 import { getRawLines, processRawLines } from './utils/cleaning.js'
-// import { createTable } from './db'
+import { createTable } from './db'
 
 function readCSV (path) {
   const rawString = readFileSync(path).toString()
@@ -11,63 +11,51 @@ function readCSV (path) {
     .map(strRow => strRow.split(COLUMN_JOIN_STRING))
 }
 
-function log (array) {
-  const len = array.length
-  const numberOfCycles = ((len - (len % 100)) / 100) + 1
-
-  for (let i = 0; i < numberOfCycles; i++) {
-    console.log(array.slice((i * 100), (i + 1) * 100))
-  }
-
-  console.log('-----------')
-}
-
 function cleanPage (htmlStr) {
-  console.log(htmlStr)
   const rawLines = getRawLines(htmlStr)
-  // log(rawLines)
-
   const processedLines = processRawLines(rawLines)
 
-  // return processedLines.join('\n')
+  return processedLines.join('\n')
 }
 
-function cleanLocationDate (locationDateStr) {
-  return locationDateStr.split('---')
+const yearRegex = /\d{4}$/
+
+function cleanDate (locationDateStr) {
+  const split = locationDateStr.split('---')
+
+  const split0Match = split[0].match(yearRegex)
+  if (split0Match) return split0Match[0]
+
+  const split1Match = split[1] ? split[1].match(yearRegex) : undefined
+  if (split1Match !== undefined) return split1Match[0]
 }
 
 async function main () {
-  // const db = await createTable('songs', ['artist', 'url', 'lyrics', 'location', 'date'])
+  const db = await createTable('songs', ['artist', 'url', 'lyrics', 'date'])
   const songsRaw = readCSV('./output/songsRaw.csv')
   let currentPercentage = '0%'
 
-  // for (let i = 608; i < 609; i++) {
   for (let i = 0; i < songsRaw.length; i++) {
     const [artist, url, rawText, rawLocationDate] = songsRaw[i]
 
-    if (url !== 'https://genius.com/Public-enemy-she-watch-channel-zero-lyrics') {
-      continue
+    const lyrics = cleanPage(rawText)
+    const date = cleanDate(rawLocationDate)
+
+    if (date) {
+      await db.insert({
+        artist,
+        url,
+        lyrics,
+        date
+      })
     }
 
-    // const lyrics = cleanPage(rawText)
-    console.log(i)
-    throw new Error('CHEEKY BREEKY')
-    // const [location, date] = cleanLocationDate(rawLocationDate)
+    const percentage = `${Math.round(((i + 1) / (songsRaw.length + 1)) * 100)}%`
 
-    // await db.insert({
-    //   artist,
-    //   url,
-    //   lyrics,
-    //   location,
-    //   date
-    // })
-
-    // const percentage = `${Math.round(((i + 1) / (songsRaw.length + 1)) * 100)}%`
-
-    // if (currentPercentage !== percentage) {
-    //   currentPercentage = percentage
-    //   console.log(currentPercentage)
-    // }
+    if (currentPercentage !== percentage) {
+      currentPercentage = percentage
+      console.log(currentPercentage)
+    }
   }
 }
 
